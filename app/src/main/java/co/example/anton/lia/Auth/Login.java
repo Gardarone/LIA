bd6b5a80f6b87e2d0c7830a5960b8d1c44c4484b
Anton <Antonb_94@hotmail.com>
2020-02-14 15:28:04 +0100

    Login

---
+++ app/src/main/java/co/example/anton/lia/Auth/Login.java
@@ -0,0 +1,15 @@
+package co.example.anton.lia.Auth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.example.anton.lia.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
